82718830580e22f3cb4ea94267bf61b5185622ee
AbazovicEdina <s2410238002@fhooe.at>
2024-10-30 15:37:44 +0100

    first quadrant

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -5,11 +5,23 @@ public class ue02_3 {
     public static void main(String[] args) {
         double radius = 4.0;
 
-        System.out.print("X-Koordinate: ");
+        // Abfrage nur für den ersten Quadranten (beide Koordinaten müssen positiv sein)
+
+        Out.print("X-Koordinate (positiv): ");
         double x = In.readDouble();
 
-        System.out.print("Y-Koordinate: ");
+        if( x < 0 ) {
+            Out.println("Eingabe ungültig!");
+            return;
+        }
+
+        Out.print("Y-Koordinate (positiv): ");
         double y = In.readDouble();
+
+        if( y < 0 ) {
+            Out.println("Eingabe ungültig!");
+            return;
+        }
 
         // c² = a² + b²
         double cQuadrat = x * x + y * y;
